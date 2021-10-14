b5410d6e3c0df6f569448ac2ab868fb07836c496
Chris Edwards <{ID}+{username}@users.noreply.github.com>
2021-10-14 11:49:11 -0500

    first commit

---
+++ random_json_keypair_generator.py
@@ -0,0 +1,29 @@
+import json
+import random
+import argparse
+
+def load_words():
+  with open('words_dictionary.json') as json_file:
+     words = json.load(json_file)
+  return words
+
+def random_word(words):
+  word = random.choice(list(words))
+  return word
+
+my_parser = argparse.ArgumentParser(description='Random JSON Key:Pair Generator')
+w
+my_parser.add_argument('Pairs', metavar='pairs', type=int, help='number of key:value pairs to generate')
+args = my_parser.parse_args()
+pairs = args.Pairs
+
+words = load_words()
+key_value = dict()
+
+for x in range(pairs):
+  key = random_word(words)
+  value = random_word(words)
+  key_value.update({key: value})
+
+jsonString = json.dumps(key_value)
+print(jsonString)
